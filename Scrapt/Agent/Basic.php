4cc694b1e9383d3e8c94573be6d89fe88a0970fc
Steven Bredenberg <steveb@wpiinc.com>
2010-10-16 15:09:20 -0400

    cURL and not-cURL

---
+++ Scrapt/Agent/Basic.php
@@ -0,0 +1,26 @@
+<?php
+
+class Scrapt_Agent_Basic extends Scrapt_Agent
+{
+	public function request($method, $url, array $params=array())
+	{
+		$options = array('http'=>array(
+			'user_agent'    => $this->userAgent,
+			'max_redirects' => $this->maxRedirects,
+			'timeout'       => $this->timeOut
+		));
+		$context = stream_context_create( $options );
+		$page    = @file_get_contents( $url, false, $context );
+	 
+		if ($method == self::METHOD_GET) {
+			
+		}
+		$data = file_get_contents($url, null, $options);
+		
+		return array('headers'=>$http_response_header,
+			 'data'=>trim($data), 
+			 'info'=>array()
+			 'redirects'=>$redirect_headers
+			 );
+	}
+}
