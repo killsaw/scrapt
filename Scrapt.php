1931daaae2254aa95c08aa4c244dd3dd2ceb18ee
Steven Bredenberg <steveb@wpiinc.com>
2010-10-16 15:08:33 -0400

    Using broken out agents

--- Scrapt.php
+++ Scrapt.php
@@ -1,16 +1,26 @@
 <?php
 
 require_once __DIR__.'/Scrapt/Webpage.php';
+require_once __DIR__.'/Scrapt/Agent.php';
+require_once __DIR__.'/Scrapt/Agent/cURL.php';
+require_once __DIR__.'/Scrapt/Agent/Basic.php';
 require_once __DIR__.'/vendor/simple_html_dom.php';
 
 class Scrapt
 {
 	protected $url;
-	protected $cacheDuration = 600; // 10 minutes.
+	protected $data;
+	protected $cacheDuration = 1200; // 20 minutes.
 
 	public function __construct($url=null)
 	{
 		$this->url = $url;
+		$this->setAgent(new Scrapt_Agent_cURL);
+	}
+	
+	public function setAgent(Scrapt_Agent $a)
+	{
+		$this->agent = $a;
 	}
 	
 	public function setURL($url)
@@ -29,7 +39,8 @@ class Scrapt
 						__DIR__, md5($this->url));
 		
 		if (!file_exists($cache_path) || $this->cacheIsOld($cache_path)) {
-			$this->data = file_get_contents($this->url);
+			$reply = $this->agent->get($this->url);
+			$this->data = $reply['data'];
 			file_put_contents($cache_path, $this->data);
 		}
 		$this->data = file_get_contents($cache_path);
